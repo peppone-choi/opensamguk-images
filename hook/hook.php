deb25440e0a3244189e9426ea0b9198c22501bbe
hide_d <hided62@gmail.com>
2018-06-15 00:28:51 +0900

    list.json 생성 코드 추가

--- hook/hook.php
+++ hook/hook.php
@@ -13,3 +13,17 @@ if($valid_hmac != $req_hmac){
 }
 
 exec("git pull -q 2>&1", $output);
+
+exec("git ls-files -z ../icons", $raw_img_list);
+$raw_img_list = explode("\x00", $raw_img_list[0]);
+$img_list = [];
+foreach ($raw_img_list as $path) {
+    $pos = strpos($path, '../icons/');
+    if($pos === false){
+        continue;
+    }
+    $path = substr($path, $pos + 9);
+    $img_list[] = $path;
+}
+
+file_put_contents('list.json', json_encode($img_list,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
